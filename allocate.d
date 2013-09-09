45ef44c5b3e7a6276b84769f521536566d7c007a
Eric Chapweske <eac@zendesk.com>
2013-09-09 11:12:29 -0700

    Fix allocate.d to properly track all allocations

--- allocate.d
+++ allocate.d
@@ -1,11 +1,27 @@
 #!/usr/sbin/dtrace -q -s
 
-ruby*:::*-create
+ruby*:::object-create
 {
-  @a[copyinstr(arg0), copyinstr(arg1), arg2] = count();
+  @o[copyinstr(arg0), copyinstr(arg1), arg2] = count();
+}
+
+ruby*:::array-create
+{
+  @a["Array", arg0, copyinstr(arg1), arg2] = count();
+}
+
+ruby*:::hash-create
+{
+  @a["Hash", arg0, copyinstr(arg1), arg2] = count();
+}
+
+ruby*:::string-create
+{
+  @a["String", arg0, copyinstr(arg1), arg2] = count();
 }
 
 END
 {
-  printa("%@8u %s %s:%d\n", @a);
+  printa("%@8u %s %s:%d\n", @o);
+  printa("%@8u %s(%d) %s:%d\n", @a);
 }
